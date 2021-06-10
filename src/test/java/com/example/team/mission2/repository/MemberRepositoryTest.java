235505a814c217de23e787301e18a195da6d64b6
JeongPyo <jeongpyo_park@tmax.co.kr>
2021-06-10 18:06:49 +0900

    test 관련 lib 추가

--- src/test/java/com/example/team/mission2/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/team/mission2/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.team.mission2.repository;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
