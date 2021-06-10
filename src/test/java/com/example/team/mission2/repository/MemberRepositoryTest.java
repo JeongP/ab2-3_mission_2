3a25e82e867ab16b2105e14dc4cef8a64be22aa7
JeongPyo <jeongpyo_park@tmax.co.kr>
2021-06-10 18:17:28 +0900

    db 접속정보 수정

--- src/test/java/com/example/team/mission2/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/team/mission2/repository/MemberRepositoryTest.java
@@ -1,8 +1,13 @@
 package com.example.team.mission2.repository;
 
+import com.example.team.mission2.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -10,4 +15,20 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class MemberRepositoryTest {
 
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() {
+        Member member = new Member();
+        member.setUsername("Mem1");
+
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+    }
 }
